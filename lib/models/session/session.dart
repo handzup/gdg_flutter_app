4c0a5f68bfd10d7ea66d1849d2dd215696a09788
shaxzodik1@gmail.com <shaxzodik1@gmail.com>
2020-09-30 20:37:19 +0500

    cc

--- lib/models/session/session.dart
+++ lib/models/session/session.dart
@@ -1,18 +1,30 @@
-import 'package:gdg_flutter_app/models/speaker/speaker.dart';
+import 'package:hive/hive.dart';
 import 'package:json_annotation/json_annotation.dart';
+
+import '../speaker/speaker.dart';
 
 part 'session.g.dart';
 
+@HiveType(typeId: 1)
 @JsonSerializable()
 class Session {
+  @HiveField(0)
   final int id;
+  @HiveField(1)
   final String complexity;
+  @HiveField(2)
   final String description;
+  @HiveField(3)
   final String presentation;
+  @HiveField(4)
   @JsonKey(fromJson: _fromJson)
+  @HiveField(5)
   List<Speaker> speakers;
+  @HiveField(6)
   final List<String> tags;
+  @HiveField(7)
   final String title;
+  @HiveField(0)
   final String videoId;
 
   Session(
